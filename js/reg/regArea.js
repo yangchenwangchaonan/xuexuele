5d99ae61c63281c7cb5454603ed2367f46f11587
liyueyue123 <15901017921@163.com>
2018-10-11 15:36:40 +0800

    li

--- js/reg/regArea.js
+++ js/reg/regArea.js
@@ -16,6 +16,89 @@ $(function () {
         $(".area-box1").hide();
         $(".area-box2").show();
         $("#search-text2").focus();
+        $("#search-text2").bind("input", function () {
+            var val = $("#search-text2").val();
+            if (val != "") {
+                city(val);
+                $("#search-close").show();
+                $(".city-international").hide();
+            } else {
+                $("#search-close").hide();
+            }
+        });
     });
-
 });
+
+// 搜索城市
+function city(val) {
+    $.ajax({
+        type: "GET",
+        url: APP_URL + "/api/User/CityList",
+        data: {
+            city: val
+        },
+        dataType: "json",
+        success: function (res) {
+            console.log(res);
+            var data = res.data;
+            var str = "";
+            $.each(data, function (index, val) {
+                str += `
+                 <li data-name="${val.name}">${val.name}</li>
+                 `;
+            });
+            $(".search-list>ul").html(str);
+            $(".search-list>ul>li").click(function () {
+                var $val = $(this).attr("data-name");
+                $("#search-text2").val($val);
+                $(".search-list").hide();
+            });
+            // 搜索
+            $(".city-search-btn").click(function () {
+                var city = $("#search-text2").val();
+                if (val != "") {
+                    citySearch(city);
+                }
+            });
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    });
+}
+
+//搜索结果
+function citySearch(city) {
+    $.ajax({
+        type: "GET",
+        url: APP_URL + "/api/User/CityList",
+        data: {
+            city: city
+        },
+        dataType: "json",
+        success: function (res) {
+            console.log(res);
+            if (res.data.length != 0) {
+                var str = "";
+                $.each(res.data, function (index, val) {
+                    str += `
+                     <li class="search-res" data-val="${val.name}">${val.name}</li>
+                     `;
+                });
+                $(".search-text>ul").html(str);
+                $(".search-text>ul>li").click(function () {
+                    var val = $(this).attr("data-val");
+                    $(".area-body").hide();
+                    $(".index-container").show();
+                    $("#reg-area").html(val);
+                });
+            } else {
+                $(".search-text").html("");
+                $(".city-international").show();
+            }
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    });
+}
