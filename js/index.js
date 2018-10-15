9832093eed3416263a1e1ea7504997e47fce1ec3
yangchenwangchaonan <946754060@qq.com>
2018-10-15 15:23:47 +0800

    yyy

--- js/index.js
+++ js/index.js
@@ -1,12 +1,59 @@
 $(function () {
     var preload;
     var mainfest;
-
     // 初始化预加载manifest清单
     function setupManifest() {
-        mainfest = [{
-                src: "http://www.taobao.com"
-            },
+        var path = "./images/"
+        console.log(path)
+        mainfest = [
+            {src: path+"01.png"},
+            {src: path+"02.png"},
+            {src: path+"03.png"},
+            {src: path+"04.png"},
+            {src: path+"10.png"},
+            {src: path+"25.png"},
+            {src: path+"36.png"},
+            {src: path+"37.png"},
+            {src: path+"38.png"},
+            {src: path+"47.png"},
+            {src: path+"60.png"},
+            {src: path+"70.png"},
+            {src: path+"102.png"},
+            {src: path+"103.png"},
+            {src: path+"104.png"},
+            {src: path+"106.png"},
+            {src: path+"112.png"},
+            {src: path+"114.png"},
+            {src: path+"120.png"},
+            {src: path+"121.png"},
+            {src: path+"123.png"},
+            {src: path+"126.png"},
+            {src: path+"127.png"},
+            {src: path+"128.png"},
+            {src: path+"145.png"},
+            {src: path+"159.png"},
+            {src: path+"169.png"},
+            {src: path+"177.png"},
+            {src: path+"198.png"},
+            {src: path+"200.png"},
+            {src: path+"218.png"},
+            {src: path+"220.png"},
+            {src: path+"229.png"},
+            {src: path+"231.png"},
+            {src: path+"236.png"},
+            {src: path+"others.jpg"},
+
+
+
+            // {src: path+"01.png"},
+            // {src: path+"01.png"},
+            // {src: path+"01.png"},
+            // {src: path+"01.png"},
+            // {src: path+"01.png"},
+            // {src: path+"01.png"},
+            // {src: path+"01.png"},
+            // {src: path+"01.png"},
+            // {src: path+"01.png"},
         ];
     }
     // 预加载函数
@@ -42,7 +89,7 @@ $(function () {
     function loadComplete() {
         // 设置定时器，当全部加载完毕后让100%停留0.4秒，提高用户体验，不至于让用户感觉不到
         window.setTimeout(() => {
-            $(window).attr("location", "http://www.taobao.com");
+            $(window).attr("location", "./html/login/login.html");
         }, 400);
 
     }
