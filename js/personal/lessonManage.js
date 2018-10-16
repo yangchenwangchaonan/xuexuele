700ce892d6b657a323b35b6b2380a3bf2e229f6c
liyueyue123 <15901017921@163.com>
2018-10-16 18:36:59 +0800

    li

--- js/personal/lessonManage.js
+++ js/personal/lessonManage.js
@@ -1,5 +1,26 @@
 $(function(){
+    lessonManageList ()
     $(".album-add").click(function(){
         $(window).attr("location","./lesson-add.html");
     });
 });
+
+function lessonManageList (){
+    var uId = sessionStorage.getItem("uid");
+    $.ajax({
+        type: "GET",
+        url: APP_URL + "/api/My/AlbumManageList",
+        data: {
+            uid:1
+        },
+        dataType: "json",
+        success: function (res) {
+            console.log(res);
+            var data = res.data;
+            $(".manage-top>span").html("x"+data.wisdombean);
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+}
--- js/personal/myLesson.js
+++ js/personal/myLesson.js
@@ -1,21 +1,40 @@
 $(function () {
     lessonList();
-    $(".table-lesson").click(function () {
-        $(window).attr("location", "../lessonDetail/lesson-detail.html");
-    });
 });
 
 function lessonList() {
     var uId = sessionStorage.getItem("uid");
     $.ajax({
-        type: "method",
+        type: "get",
         url: APP_URL + "/api/My/AlbumList",
         data: {
-            uid: uId
+            uid: uId    
         },
         dataType: "json",
         success: function (res) {
             console.log(res);
+            var data = res.data;
+            var str = "";
+            $.each(data, function (index, val) {
+                str += `
+                 <li data-cId="${val.id}">
+                    <div class="table-lesson">
+                        <img src="${val.albumimg}" />
+                        <div class="lesson-name">${val.albumname}</div>
+                        <div class="lesson-lock"><img src="../../images/222.png" />${val.unlocknum}</div>
+                    </div>
+                </li>
+                 `;
+            });
+            $(".mylesson-list").html(str);
+            $(".mylesson-list>li").click(function () {
+                var lessonId = $(this).attr("data-cId");
+                alert(lessonId);
+                $(window).attr("location", "../lessonDetail/lesson-detail.html?lessonId="+lessonId);
+            });
+        },
+        error: function (err) {
+            console.log(err);
         }
     });
 }
