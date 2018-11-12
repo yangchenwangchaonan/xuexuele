fb5ac940a36ef561a490551ef769161b78799100
yangchenwangchaonan <946754060@qq.com>
2018-11-12 18:24:40 +0800

    yyy

--- js/common/common.js
+++ js/common/common.js
@@ -1,7 +1,11 @@
 APP_URL = 'https://xuexuele.huimor.com';
 
-window.onload = function(){
-    document.body.addEventListener("touchmove",function(event){
-        event.preventDefault();
-        });
-};
+
+$(document).ready(function(){  
+	console.log(122555)
+    function stopScrolling( touchEvent ) {   
+        touchEvent.preventDefault();   
+    }  
+    document.addEventListener( 'touchstart' , stopScrolling , false );  
+    document.addEventListener( 'touchmove' , stopScrolling , false );  
+});
