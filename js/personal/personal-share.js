5891a882b9d16df22459ec2b2eed87bf8ce5d3a1
liyueyue123 <15901017921@163.com>
2018-11-19 18:16:53 +0800

    li

--- js/personal/personal-share.js
+++ js/personal/personal-share.js
@@ -1,27 +1,10 @@
 $(function () {
-    var url = window.location.href.split("#")[0];
-    var courseId = url.split("&")[0].split("=")[1];
-    console.log(courseId);
-    $.ajax({
-        type: "GET",
-        url: APP_URL + "/api/Wisdom/WisdomDetailShare",
-        data: {
-            courseid: courseId
-        },
-        dataType: "json",
-        success: function (res) {
-            console.log(res);
-            if (res.code == 1) {
-                var courseName = res.data.coursename;
-                var courseContent = res.data.coursetxt;
-                WxConfig(url,courseId, courseName, courseContent); //获取接口权限
-            }
-        }
-    });
+    WxConfig(); //获取接口权限
 });
 
 // 获取公众号接口权限
-function WxConfig(url, courseId,courseName, courseContent) {
+function WxConfig() {
+    var url = window.location.href.split("#")[0];
     $.ajax({
         type: "POST",
         url: APP_URL + "/api/My/WxConfig",
@@ -55,9 +38,9 @@ function WxConfig(url, courseId,courseName, courseContent) {
                     console.log("ready");
                     //分享给朋友
                     wx.onMenuShareAppMessage({
-                        title: '学学乐课程:' + courseName,
-                        desc: courseContent,
-                        link: 'https://m.xuexuele.huimor.com/html/lessonDetail/lesson-share.html?courseId=' + courseId,
+                        title: '学学乐',
+                        desc: '一起闯关，共同学习',
+                        link: 'https://m.xuexuele.huimor.com/signOut.html',
                         imgUrl: 'https://m.xuexuele.huimor.com/images/04.png', // 分享图标
                     }, function (res) {
                         //这里是回调函数
@@ -66,25 +49,25 @@ function WxConfig(url, courseId,courseName, courseContent) {
 
                     //分享到朋友圈
                     wx.onMenuShareTimeline({
-                        title: '学学乐课程:' + courseName, // 分享标题
-                        link: 'https://m.xuexuele.huimor.com/html/lessonDetail/lesson-share.html?courseId=' + courseId,
+                        title: ' 学学乐', // 分享标题
+                        link: 'https://m.xuexuele.huimor.com/signOut.html',
                         imgUrl: 'https://m.xuexuele.huimor.com/images/04.png', // 分享图标
                     }, function (res) {
                         console.log(res);
                     });
                     // 分享到QQ
                     wx.onMenuShareQQ({
-                        title: '学学乐课程:' + courseName, // 分享标题
-                        desc: courseContent, // 分享描述
-                        link: 'https://m.xuexuele.huimor.com/html/lessonDetail/lesson-share.html?courseId=' + courseId, // 分享链接
+                        title: ' 学学乐', // 分享标题
+                        desc: '一起闯关，共同学习', // 分享描述
+                        link: 'https://m.xuexuele.huimor.com/signOut.html', // 分享链接
                         imgUrl: 'https://m.xuexuele.huimor.com/images/04.png', // 分享图标
                     }, function (res) {
                         console.log(res);
                     });
                     // 分享到QQ空间
                     wx.onMenuShareQZone({
-                        title: '学学乐课程:' + courseName, // 分享标题
-                        link: 'https://m.xuexuele.huimor.com/html/lessonDetail/lesson-share.html?courseId=' + courseId,
+                        title: ' 学学乐', // 分享标题
+                        link: 'https://m.xuexuele.huimor.com/signOut.html',
                         imgUrl: 'https://m.xuexuele.huimor.com/images/04.png', // 分享图标
                     }, function (res) {
                         console.log(res);
